523429675604fee8fc5def55a46fa33300c7451f
ola wozniak <olawozniak551@gmail.com>
2022-06-07 15:42:02 +0200

    task 27.5

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+
+const testimonialsRoutes = require('./routes/testimonials.routes.js');
+const concertsRoutes = require('./routes/concerts.routes.js');
+const seatsRoutes = require('./routes/seats.routes.js');
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.listen(8009, () => {
+  console.log('Server is running on port: 8009');
+});
+
+app.use('/api', testimonialsRoutes);
+app.use('/api', concertsRoutes);
+app.use('/api', seatsRoutes);
+
+app.use((req, res) => {
+  res.status(404).json({message: '404 not found...'});
+});
